b2e13daf1b3650d4b127f5ae905c96084e172d57
Simanta Paul <simanta.paul@paysera.net>
2024-05-26 17:29:28 +0600

    Initialized project

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
