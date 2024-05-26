b0582bcddf126bf1c2b93adde65fa4c21de13091
Simanta Paul <simanta.paul@paysera.net>
2024-05-26 22:11:46 +0600

    Db connection

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,28 @@
-const sayHello = (name: 'Simanta') => {
-  console.log(name);
-};
+import express, { Response, Request } from 'express';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+dotenv.config();
 
-sayHello('Simanta');
+import './services/db.service';
+
+const app = express();
+
+if (app.get('env') === 'development') {
+  app.use(morgan('tiny'));
+}
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  return res.status(200).send({ message: 'IC E-com API' });
+});
+
+app.use((req: Request, res: Response) => {
+  return res.status(404).send({
+    message: 'Not found!',
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
---
+++ src/services/db.service.ts
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose';
+
+mongoose
+  .connect(process.env.DB_URL as string)
+  .then(() => console.log('Db connected successfully!'))
+  .catch((err) => {
+    throw new Error(err);
+  });
